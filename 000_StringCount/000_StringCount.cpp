83f13fcdf85ac7f815ffe3390daa2b970dac5705
kcs7066 <kcs7066@naver.com>
2024-09-23 15:48:47 +0900

    김창수 : 번호 수정

---
+++ 000_StringCount/000_StringCount.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+// 지금만들라고 하는 함수들은
+// strlen
+
+int StringCount(const char* const _Str)
+{
+    int Count = 0;
+
+    while (_Str[Count])
+    {
+        Count += 1;
+    }
+
+    return Count;
+}
+
+int main()
+{
+    // [B][B][B][B][0]
+
+    int Count = StringCount("BBBB");
+
+    int a = 0;
+}
