0e274e9c8041fcae9cdaef6c8812421fd4a776b3
kcs7066 <kcs7066@naver.com>
2024-10-07 12:27:51 +0900

    김창수 : 업데이트

--- TextRpg003ClassFile/Town.cpp
+++ TextRpg003ClassFile/Town.cpp
@@ -20,16 +20,9 @@ int UTown::InPlayer(class UPlayer& _Player)
 
 		switch (Select)
 		{
-		case '0':
-			printf_s("0눌렀습니다.");
-			_getch();
-			break;
-		case '1':
-			printf_s("1눌렀습니다.");
-			_getch();
-			break;
 		case '2':
 		{
+			_Player.SetCurZone(Select - '0');
 			// UFightzone FightZone;
 
 			printf_s("2눌렀습니다.");
@@ -37,10 +30,12 @@ int UTown::InPlayer(class UPlayer& _Player)
 			return 2;
 			break;
 		}
+		case '0':
+		case '1':
 		case '3':
-			printf_s("3눌렀습니다.");
+			printf_s("아직 완성되지 않은 기능입니다.");
 			_getch();
-			return 3;
+			return 1;
 		default:
 			break;
 		}
